7854cebe97e0ff35546d68c54a211a8cfd4dbdb4
Crims0neclipse <Crims0neclipse@users.noreply.gitgub.com>
2019-02-04 11:28:27 -0400

    Added source file

--- Lab1.cpp
+++ Lab1.cpp
@@ -6,12 +6,15 @@ using namespace std;
 int main(){
 	
 	vector <char> Vcomp;
-	vector <int> V2 ={10, 14, 32, 64,  16};
+	vector <int> V2 ={10,14,32,64,16};
 	
 	Vcomp. push_back('P'); // add new element
 	V2.pop_back(); // remove last element
 	
-	for (int i=0; i< V2.size(); i++)
-	   cout << V2[i] << "";
+	/*for (int i=0; i< V2.size(); i++)
+	   cout << V2[i] << ""; */
+	   
+	for (auto v: V2) // for each value in V2
+	    cout << v << "";   
 	   
    }
